fde846fa660420f8b5804cd16e66ea8c68ddf9c4
maringeles <angeles.mpa20@gmail.com>
2024-03-23 19:00:15 -0400

    Agregar archivos de proyecto.

---
+++ ejerciciosAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ejerciciosAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
